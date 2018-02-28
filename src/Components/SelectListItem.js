07119760bd6bf411815d2a14ba4c09ed7d24fd41
Hugh Bellamy <hughbellars@gmail.com>
2018-02-28 16:07:53 +0000

    Cleanups

--- src/Components/SelectListItem.js
+++ src/Components/SelectListItem.js
@@ -4,16 +4,16 @@ import makeLink from '../Utilities/MakeLink';
 import './css/SelectListItem.css';
 
 const SelectListItem = ({ selected, showImage, baseLink, name }) => (
-    <li className={`${selected ? 'selected ' : ''}select-list-item`} >
-      {showImage &&
-        <img
-          src={require(`../Content/Logos/${name}.png`)}
-          width="75"
-          height="75"
-          alt={name}
-        />
-      }
-      <Link to={`${baseLink}/${makeLink(name)}`}>{name}</Link>
-    </li>
-)
+  <li className={`${selected ? 'selected ' : ''}select-list-item`} >
+    {showImage &&
+      <img
+        src={require(`../Content/Logos/${name}.png`)}
+        width="75"
+        height="75"
+        alt={name}
+      />
+    }
+    <Link to={`${baseLink}/${makeLink(name)}`}>{name}</Link>
+  </li>
+);
 export default SelectListItem;
