1e9408c36a274172e5e5b2cf75306fc332d117b0
Hugh Bellamy <hughbellars@gmail.com>
2018-02-28 16:40:45 +0000

    Cleanups to the Personal section

--- src/Components/CV/CVDetail.js
+++ src/Components/CV/CVDetail.js
@@ -4,7 +4,7 @@ import './css/CVDetail.css';
 const CVDetail = ({ title, children }) => (
   <div className="cv-detail">
     <h3 className="cv-detail-title">{title}</h3>
-    <p className="cv-detail-description">{children}</p>
+    {children}
   </div>
 );
 export default CVDetail;
