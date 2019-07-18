28f165074fbe9c783964a33e43978f2aeb3faf39
Hugh Bellamy <hughbellars@gmail.com>
2019-07-18 23:21:08 +0100

    Update

--- src/Components/CV/AboutSection.js
+++ src/Components/CV/AboutSection.js
@@ -4,7 +4,7 @@ import './css/AboutSection.css';
 
 const AboutSection = () => (
   <CVSection title="Hi. I'm Hugh" id="About">
-      <p className="introduction-text"><span>I'm a 20 year old student studying Philosophy, Politics and Economics at Oxford University. In my spare time I develop apps, hack on side projects and contribute to open source projects.</span></p>
+      <p className="introduction-text"><span>I'm a student studying Philosophy, Politics and Economics at Oxford University. In my spare time I develop apps, hack on side projects and contribute to open source projects.</span></p>
   </CVSection>
 );
 export default AboutSection;
